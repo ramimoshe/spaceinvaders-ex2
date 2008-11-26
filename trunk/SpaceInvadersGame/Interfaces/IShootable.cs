4b9777e47a9eb7d03cdaf2e395e57d442e3aa312
kobyahron <kobyahron@d834c534-b8d4-11dd-940c-8b96d25ca2fe>
2008-11-26 11:36:20 +0000

    Add remarks and fix stylecop errors

--- trunk/SpaceInvadersGame/Interfaces/IShootable.cs
+++ trunk/SpaceInvadersGame/Interfaces/IShootable.cs
@@ -4,8 +4,14 @@ using System.Text;
 
 namespace SpaceInvadersGame.Interfaces
 {
+    /// <summary>
+    /// Interface representing a a component that has an abillity to shoot
+    /// </summary>
     public interface IShootable
-    {        
+    {    
+        /// <summary>
+        /// Relase a shoot from the component
+        /// </summary>
         void    Shoot();
     }
 }
