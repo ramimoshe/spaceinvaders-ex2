fe6cf0ba144b42280835c5bc09f3613b354b97a2
kobyahron <kobyahron@d834c534-b8d4-11dd-940c-8b96d25ca2fe>
2009-01-07 10:45:27 +0000

    Seperate the cube into 2 different types

--- A09 Ex05 Koby 021766944 Inbar 015267479/Dreidel/ObjectModel/CubeTexture.cs
+++ A09 Ex05 Koby 021766944 Inbar 015267479/Dreidel/ObjectModel/CubeTexture.cs
@@ -12,31 +12,20 @@ using Microsoft.Xna.Framework.Storage;
 
 namespace DreidelGame.ObjectModel
 {
-    public class CubeTexture : CompositeGameComponent
-    {
-        private Vector3[] m_VerticesCoordinates = new Vector3[8];
-        private const float k_ZFactorWidth = 6;
-        private const float k_ZFactorCoordinate = 3f;     
-        private readonly Color r_UpDownColor = Color.BurlyWood;
-
-        private const int k_ZFactor = 8;
-
+    /// <summary>
+    /// A cube that contains Texture vertices
+    /// </summary>
+    public class CubeTexture : Cube
+    {       
         public CubeTexture(Game i_Game) : base(i_Game)
         {
         }
 
-        public override void Initialize()
-        {
-            // TODO: Move to a parent class
-            m_VerticesCoordinates[0] = new Vector3(-3, -3, k_ZFactorCoordinate);
-            m_VerticesCoordinates[1] = new Vector3(-3, 3, k_ZFactorCoordinate);
-            m_VerticesCoordinates[2] = new Vector3(3, 3, k_ZFactorCoordinate);
-            m_VerticesCoordinates[3] = new Vector3(3, -3, k_ZFactorCoordinate);
-            m_VerticesCoordinates[4] = new Vector3(3, -3, k_ZFactorCoordinate - k_ZFactorWidth);
-            m_VerticesCoordinates[5] = new Vector3(3, 3, k_ZFactorCoordinate - k_ZFactorWidth);
-            m_VerticesCoordinates[6] = new Vector3(-3, 3, k_ZFactorCoordinate - k_ZFactorWidth);
-            m_VerticesCoordinates[7] = new Vector3(-3, -3, k_ZFactorCoordinate - k_ZFactorWidth);            
-
+        /// <summary>
+        /// Creates the cube sides as VertexPositionTexture
+        /// </summary>
+        protected override void     CreateSides()
+        {          
             // Creating the front side
             Add(new Side<VertexPositionTexture>(
                             Game,
@@ -46,8 +35,7 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionTexture(m_VerticesCoordinates[0], new Vector2(0, .5f)),
                             new VertexPositionTexture(m_VerticesCoordinates[1], new Vector2(0, 0)),
                             new VertexPositionTexture(m_VerticesCoordinates[2], new Vector2(.5f, 0)),
-                            new VertexPositionTexture(m_VerticesCoordinates[3], new Vector2(.5f, .5f))
-                            ));
+                            new VertexPositionTexture(m_VerticesCoordinates[3], new Vector2(.5f, .5f))));
 
             // Creating the back side
             Add(new Side<VertexPositionTexture>(
@@ -58,8 +46,7 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionTexture(m_VerticesCoordinates[4], new Vector2(0, 1)),
                             new VertexPositionTexture(m_VerticesCoordinates[5], new Vector2(0, .5f)),
                             new VertexPositionTexture(m_VerticesCoordinates[6], new Vector2(.5f, .5f)),
-                            new VertexPositionTexture(m_VerticesCoordinates[7], new Vector2(.5f, 1))
-                            ));
+                            new VertexPositionTexture(m_VerticesCoordinates[7], new Vector2(.5f, 1))));
 
             // Creating the right side
             Add(new Side<VertexPositionTexture>(
@@ -70,8 +57,7 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionTexture(m_VerticesCoordinates[3], new Vector2(.5f, 1)),
                             new VertexPositionTexture(m_VerticesCoordinates[2], new Vector2(.5f, .5f)),
                             new VertexPositionTexture(m_VerticesCoordinates[5], new Vector2(1, .5f)),
-                            new VertexPositionTexture(m_VerticesCoordinates[4], new Vector2(1, 1))
-                            ));
+                            new VertexPositionTexture(m_VerticesCoordinates[4], new Vector2(1, 1))));
 
             // Creating the left side
             Add(new Side<VertexPositionTexture>(
@@ -82,8 +68,7 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionTexture(m_VerticesCoordinates[7], new Vector2(.5f, .5f)),
                             new VertexPositionTexture(m_VerticesCoordinates[6], new Vector2(.5f, 0)),
                             new VertexPositionTexture(m_VerticesCoordinates[1], new Vector2(1, 0)),
-                            new VertexPositionTexture(m_VerticesCoordinates[0], new Vector2(1, .5f))
-                            ));
+                            new VertexPositionTexture(m_VerticesCoordinates[0], new Vector2(1, .5f))));
 
             // Creating the top side
             Add(new Side<VertexPositionColor>(
@@ -94,8 +79,7 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionColor(m_VerticesCoordinates[1], r_UpDownColor),
                             new VertexPositionColor(m_VerticesCoordinates[6], r_UpDownColor),
                             new VertexPositionColor(m_VerticesCoordinates[5], r_UpDownColor),
-                            new VertexPositionColor(m_VerticesCoordinates[2], r_UpDownColor)
-                            ));
+                            new VertexPositionColor(m_VerticesCoordinates[2], r_UpDownColor)));
 
             // Creating the bottom side
             Add(new Side<VertexPositionColor>(
@@ -106,21 +90,15 @@ namespace DreidelGame.ObjectModel
                             new VertexPositionColor(m_VerticesCoordinates[3], r_UpDownColor),
                             new VertexPositionColor(m_VerticesCoordinates[4], r_UpDownColor),
                             new VertexPositionColor(m_VerticesCoordinates[7], r_UpDownColor),
-                            new VertexPositionColor(m_VerticesCoordinates[0], r_UpDownColor)
-                            ));
-
-            base.Initialize();
+                            new VertexPositionColor(m_VerticesCoordinates[0], r_UpDownColor)));
         }
 
-        protected override void  LoadContent()
+        /// <summary>
+        /// Loads the texture we want to draw on the cube sides
+        /// </summary>
+        protected override void     LoadContent()
         {           
             Texture = Game.Content.Load<Texture2D>(@"Sprites\Dreidel");
-        }
-
-        // TODO: Remove
-        public override void    Draw(GameTime gameTime)
-        {
-            base.Draw(gameTime);
         }
     }
 }
